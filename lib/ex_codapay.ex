fe0c701c48631fc9f99891ccc0895d1f52927cea
TamakiMaeda <giraph.me@gmail.com>
2018-05-10 21:58:17 +0900

    First commit

---
+++ lib/ex_codapay.ex
@@ -0,0 +1,18 @@
+defmodule ExCodapay do
+  @moduledoc """
+  Documentation for ExCodapay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCodapay.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_codapay_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCodapayTest do
+  use ExUnit.Case
+  doctest ExCodapay
+
+  test "greets the world" do
+    assert ExCodapay.hello() == :world
+  end
+end
