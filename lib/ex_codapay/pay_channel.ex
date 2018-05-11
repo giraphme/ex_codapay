b6396f47cb41d912065066495d2fb3bc09188bb5
TamakiMaeda <giraph.me@gmail.com>
2018-05-11 19:18:11 +0900

    Add ExCodapay.PayChannel

---
+++ lib/ex_codapay/pay_channel.ex
@@ -0,0 +1,43 @@
+defmodule ExCodapay.PayChannel do
+  @supported_all [
+    {1, "BCA"},
+    {2, "BRI"},
+    {3, "BII"},
+    {4, "BNI"},
+    {5, "CIMB"},
+    {6, "Mandiri"},
+    {7, "Permata"},
+    {8, "Other Banks"},
+    {9, "Mobile Banking BCA"},
+    {12, "Mobile Banking BRI"},
+    {13, "Mobile Banking BII"},
+    {14, "Mobile Banking BNI"},
+    {15, "Mobile Banking CIMB"},
+    {16, "Mobile Banking Mandiri"},
+    {17, "Mobile Banking Permata"},
+    {18, "Mobile Banking Other Banks"},
+    {55, "Alfamart"},
+    {56, "7-Eleven"},
+    {77, "Indomaret"}
+  ]
+  @indonesia_numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 55, 56, 77]
+  @indonesia_bank_numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18]
+  @indonesia_otc_numbers [55, 56, 77]
+
+  def to_i(str) do
+    @supported_all
+    |> Enum.find(fn {_, label} -> label == str end)
+    |> elem(0)
+  end
+
+  def to_s(num) do
+    @supported_all
+    |> Enum.find(fn {key, _} -> key == num end)
+    |> elem(1)
+  end
+
+  def supported_pay_channels(country, pay_type \\ nil)
+  def supported_pay_channels(:indonesia, :bank), do: @indonesia_bank_numbers
+  def supported_pay_channels(:indonesia, :otc), do: @indonesia_otc_numbers
+  def supported_pay_channels(:indonesia, nil), do: @indonesia_numbers
+end
---
+++ test/ex_codapay/pay_channel_test.exs
@@ -0,0 +1,24 @@
+defmodule ExCodapay.PayChannelTest do
+  use ExUnit.Case
+
+  describe "ExCodapay.PayChannel.to_i/1" do
+    test "get a number" do
+      assert 7 == ExCodapay.PayChannel.to_i("Permata")
+    end
+  end
+
+  describe "ExCodapay.PayChannel.to_s/1" do
+    test "get a string" do
+      assert "Permata" == ExCodapay.PayChannel.to_s(7)
+    end
+  end
+
+  describe "ExCodapay.PayChannel.supported_pay_channels/2" do
+    test "get a list of pay_channel ids" do
+      assert channels = ExCodapay.PayChannel.supported_pay_channels(:indonesia, :otc)
+      assert is_list(channels)
+      assert channels = ExCodapay.PayChannel.supported_pay_channels(:indonesia, :bank)
+      assert is_list(channels)
+    end
+  end
+end
