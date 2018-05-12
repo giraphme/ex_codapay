38aeeeafa78845de307d0a21cdb6edc6098b82c2
TamakiMaeda <giraph.me@gmail.com>
2018-05-12 13:17:20 +0900

    Add ExCodapay.Config.base_url/1

--- lib/ex_codapay/config.ex
+++ lib/ex_codapay/config.ex
@@ -1,10 +1,20 @@
 defmodule ExCodapay.Config do
-  defstruct ~w(api_key country language pay_types pay_channels sandbox_mode)a
+  defstruct ~w(api_key country language sandbox_mode)a
+
+  def new!(overrides \\ []) do
+    {:ok, config} = new(overrides)
+    config
+  end
 
   def new(overrides \\ []) do
     struct(__MODULE__, build_params(overrides))
     |> ExCodapay.Config.Validation.validate()
   end
+
+  @sandbox_url "https://sandbox.codapayments.com/airtime/offline/charge/v2"
+  @production_url "https://airtime.codapayments.com/airtime/offline/charge/v2"
+  def base_url(%__MODULE__{sandbox_mode: true}), do: @sandbox_url
+  def base_url(%__MODULE__{sandbox_mode: false}), do: @production_url
 
   defp build_params(overrides) do
     defaults()
@@ -16,7 +26,6 @@ defmodule ExCodapay.Config do
     |> Map.merge(Enum.into(overrides, %{}))
     |> Enum.map(&assign_system_env/1)
     |> Enum.into(%{})
-    |> convert_pay_channels_to_i()
   end
 
   defp defaults do
@@ -24,8 +33,6 @@ defmodule ExCodapay.Config do
       api_key: {:system, "CODAPAY_API_KEY"},
       country: :indonesia,
       language: :in,
-      pay_types: nil,
-      pay_channels: nil,
       sandbox_mode: true
     ]
   end
@@ -36,32 +43,5 @@ defmodule ExCodapay.Config do
 
   defp assign_system_env({key, value}) do
     {key, value}
-  end
-
-  defp convert_pay_channels_to_i(%{pay_channels: pay_channels} = params)
-       when is_list(pay_channels) do
-    pay_channels =
-      Enum.map(pay_channels, fn
-        pay_channel_number when is_integer(pay_channel_number) ->
-          case ExCodapay.PayChannel.to_s(pay_channel_number) do
-            nil -> raise ArgumentError, "Not found in valid list the `#{pay_channel_number}`"
-            _ -> pay_channel_number
-          end
-
-        pay_channel when is_binary(pay_channel) ->
-          case ExCodapay.PayChannel.to_i(pay_channel) do
-            nil -> raise ArgumentError, "Not found in valid list the `#{pay_channel}`"
-            pay_channel_number -> pay_channel_number
-          end
-
-        _ ->
-          raise ArgumentError, "Not found in valid list"
-      end)
-
-    Map.put(params, :pay_channels, pay_channels)
-  end
-
-  defp convert_pay_channels_to_i(params) do
-    params
   end
 end
--- test/ex_codapay/config_test.exs
+++ test/ex_codapay/config_test.exs
@@ -5,20 +5,26 @@ defmodule ExCodapay.ConfigTest do
     test "get {:ok, %ExCodapay.Config{}}" do
       assert {:error, "Can't find api_key"} = ExCodapay.Config.new()
       System.put_env("CODAPAY_API_KEY", "API_KEY_FROM_SYSTEM_ENV")
-      assert {:error, "Can't find pay_types"} = ExCodapay.Config.new()
-      Application.put_env(:ex_codapay, :pay_types, [:invalid])
-      assert {:error, "Has unsupported pay_type"} = ExCodapay.Config.new()
-      Application.put_env(:ex_codapay, :pay_types, [:otc, :bank])
 
       assert {:ok,
               %ExCodapay.Config{
                 api_key: "API_KEY_FROM_SYSTEM_ENV",
                 country: :indonesia,
                 language: :in,
-                pay_types: [:otc, :bank],
-                pay_channels: nil,
                 sandbox_mode: true
               }} == ExCodapay.Config.new()
+    end
+  end
+
+  describe "ExCodapay.Config.base_url/1" do
+    test "get url for production if sandbox_mode is enable" do
+      expected_url = "https://airtime.codapayments.com/airtime/offline/charge/v2"
+      assert ExCodapay.Config.base_url(%ExCodapay.Config{sandbox_mode: false}) == expected_url
+    end
+
+    test "get url for sandbox if sandbox_mode is disable" do
+      expected_url = "https://sandbox.codapayments.com/airtime/offline/charge/v2"
+      assert ExCodapay.Config.base_url(%ExCodapay.Config{sandbox_mode: true}) == expected_url
     end
   end
 end
