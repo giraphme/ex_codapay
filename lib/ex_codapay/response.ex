4133c4e0f288bc718edcf68fe6f280db264af79d
TamakiMaeda <giraph.me@gmail.com>
2018-05-12 19:59:05 +0900

    Add response parser for Codapay API

---
+++ lib/ex_codapay/response.ex
@@ -0,0 +1,45 @@
+defmodule ExCodapay.Response do
+  defstruct [
+    :txn_id,
+    :txn_timeout,
+    :result_code,
+    :result_description,
+    :pay_instructions,
+    :total_price,
+    :payment_code,
+    :profile,
+    :raw_response_body,
+    :response_status_code,
+    :request
+  ]
+
+  def from_httpostion(
+        {:ok, %HTTPoison.Response{body: body} = response},
+        %ExCodapay.Request{} = request
+      ) do
+    with {:ok, parsed_body} <- Jason.decode(body) do
+      response = %ExCodapay.Response{
+        txn_id: parsed_body["txn_id"],
+        txn_timeout: parsed_body["txn_timeout"],
+        result_code: parsed_body["result_code"],
+        result_description: parsed_body["result_desc"],
+        pay_instructions: parsed_body["pay_instructions"],
+        total_price: parsed_body["total_price"],
+        payment_code: parsed_body["profile"]["payment_code"],
+        profile: parsed_body["profile"],
+        response_status_code: response.status_code,
+        raw_response_body: body,
+        request: request
+      }
+
+      if success?(response) do
+        {:ok, response}
+      else
+        {:error, response}
+      end
+    end
+  end
+
+  def success?(%__MODULE__{result_code: 204}), do: true
+  def success?(%__MODULE__{result_code: _}), do: false
+end
