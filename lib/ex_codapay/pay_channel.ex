28d5afa649b13238b7d76aeaf389b1a2a06be611
TamakiMaeda <giraph.me@gmail.com>
2018-05-13 21:46:40 +0900

    Change response of filter_pay_channels/2

--- lib/ex_codapay/pay_channel.ex
+++ lib/ex_codapay/pay_channel.ex
@@ -1,25 +1,24 @@
 defmodule ExCodapay.PayChannel do
-  @supported_all [
-    {1, "BCA"},
-    {2, "BRI"},
-    {3, "BII"},
-    {4, "BNI"},
-    {5, "CIMB"},
-    {6, "Mandiri"},
-    {7, "Permata"},
-    {8, "Other Banks"},
-    {9, "Mobile Banking BCA"},
-    {12, "Mobile Banking BRI"},
-    {13, "Mobile Banking BII"},
-    {14, "Mobile Banking BNI"},
-    {15, "Mobile Banking CIMB"},
-    {16, "Mobile Banking Mandiri"},
-    {17, "Mobile Banking Permata"},
-    {18, "Mobile Banking Other Banks"},
-    {55, "Alfamart"},
-    {56, "7-Eleven"},
-    {77, "Indomaret"}
-  ]
+  @supported_all %{
+    1 => "BCA",
+    2 => "BRI",
+    3 => "BII",
+    4 => "BNI",
+    5 => "CIMB",
+    6 => "Mandiri",
+    7 => "Permata",
+    8 => "Other Banks",
+    9 => "Mobile Banking BCA",
+    12 => "Mobile Banking BRI",
+    13 => "Mobile Banking BII",
+    14 => "Mobile Banking BNI",
+    15 => "Mobile Banking CIMB",
+    16 => "Mobile Banking Mandiri",
+    17 => "Mobile Banking Permata",
+    18 => "Mobile Banking Other Banks",
+    55 => "Alfamart",
+    77 => "Indomaret"
+  }
   @indonesia_numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 55, 56, 77]
   @indonesia_bank_numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18]
   @indonesia_otc_numbers [55, 56, 77]
@@ -31,14 +30,18 @@ defmodule ExCodapay.PayChannel do
   end
 
   def to_s(num) do
-    @supported_all
-    |> Enum.find(fn {key, _} -> key == num end)
-    |> elem(1)
+    @supported_all[num]
   end
 
-  def filter_pay_channels(country, pay_type \\ nil)
-  def filter_pay_channels(:indonesia, :bank), do: @indonesia_bank_numbers
-  def filter_pay_channels(:indonesia, :otc), do: @indonesia_otc_numbers
-  def filter_pay_channels(:indonesia, nil), do: @indonesia_numbers
-  def filter_pay_channels(_, _), do: []
+  def filter_pay_channels(country, pay_type \\ nil) do
+    Map.take(
+      Map.new(@supported_all),
+      filter_pay_channel_ids(country, pay_type)
+    )
+  end
+
+  defp filter_pay_channel_ids(:indonesia, :bank), do: @indonesia_bank_numbers
+  defp filter_pay_channel_ids(:indonesia, :otc), do: @indonesia_otc_numbers
+  defp filter_pay_channel_ids(:indonesia, nil), do: @indonesia_numbers
+  defp filter_pay_channel_ids(_, _), do: []
 end
--- test/ex_codapay/pay_channel_test.exs
+++ test/ex_codapay/pay_channel_test.exs
@@ -16,9 +16,9 @@ defmodule ExCodapay.PayChannelTest do
   describe "ExCodapay.PayChannel.filter_pay_channels/2" do
     test "get a list of pay_channel ids" do
       assert channels = ExCodapay.PayChannel.filter_pay_channels(:indonesia, :otc)
-      assert is_list(channels)
+      assert is_map(channels)
       assert channels = ExCodapay.PayChannel.filter_pay_channels(:indonesia, :bank)
-      assert is_list(channels)
+      assert is_map(channels)
     end
   end
 end
