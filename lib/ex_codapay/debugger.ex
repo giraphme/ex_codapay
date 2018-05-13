60b040fa061576900f6414d8726e8ca4626e0945
TamakiMaeda <giraph.me@gmail.com>
2018-05-13 12:58:18 +0900

    Add dubugger for completion notification

---
+++ lib/ex_codapay/debugger.ex
@@ -0,0 +1,19 @@
+defmodule ExCodapay.Debugger do
+  def mock_completion_notification(
+        %ExCodapay.Config{} = config,
+        [txn_id: txn_id, order_id: order_id] = attrs
+      )
+      when is_list(attrs) and is_integer(txn_id) and is_binary(order_id) do
+    mock = %{
+      "TxnId" => txn_id,
+      "OrderId" => order_id,
+      "ResultCode" => attrs[:result_code] || 0,
+      "TotalPrice" => attrs[:total_price] || 0,
+      "PaymentType" => attrs[:payment_type] || 0
+    }
+
+    seed = Enum.join([mock["TxnId"], config.api_key, mock["OrderId"], mock["ResultCode"]])
+    checksum_binary = :crypto.hash(:md5, seed)
+    Map.put(mock, "Checksum", Base.encode16(checksum_binary, case: :lower))
+  end
+end
